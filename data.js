953e0bd3c089cada379b162915079adef28422a0
yuvrajv64-ops <yuvrajv64@gmail.com>
2026-04-23 18:29:45 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,22 @@
+const data = {
+  "mujhe paani chahiye": {
+    en: "I need water",
+    ta: "எனக்கு தண்ணீர் வேண்டும்"
+  },
+  "sir machine band hai": {
+    en: "Sir, the machine has stopped",
+    ta: "சார், மெஷின் நின்றுவிட்டது"
+  },
+  "material late hai": {
+    en: "Material is delayed",
+    ta: "பொருள் தாமதமாக உள்ளது"
+  },
+  "production ruk sakta hai": {
+    en: "Production may stop",
+    ta: "உற்பத்தி நிறுத்தப்படலாம்"
+  },
+  "main plant ja raha hun": {
+    en: "I am going to the plant",
+    ta: "நான் தொழிற்சாலைக்கு போகிறேன்"
+  }
+};
